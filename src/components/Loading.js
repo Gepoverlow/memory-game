ab4a87316d389b88e755adb6a61ee7d03296eff8
Gepoverlow <gepeoverload@gmail.com>
2022-04-05 11:58:13 +0200

    added some card container styling and hover animations, looks good to deploy to ghpages!

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from "react";
 const Loading = () => {
   let content = (
     <React.Fragment>
-      <h1 className="loading">LOADING</h1>
+      <h1 className="loading">LOADING...</h1>
     </React.Fragment>
   );
 
